5b3e2fceceabf29f10745eb342dcb7dd0a159ae0
Vasan Arul <vasankarul@gmail.com>
2020-06-30 16:28:34 -0700

    Committed code should reflect bundle additions to html

--- server-proxy/server/index.js
+++ server-proxy/server/index.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 const path = require('path');
 const port = 4001;
 
-app.use(cors);
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/:1', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
