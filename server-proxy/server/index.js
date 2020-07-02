e8ca0dba8439a2435d29f845cfff732356b46441
Vasan Arul <vasankarul@gmail.com>
2020-07-02 10:29:37 -0700

    System clean- submodule removal and readdition

--- server-proxy/server/index.js
+++ server-proxy/server/index.js
@@ -8,8 +8,12 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/:1', (req, res) => {
+app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
