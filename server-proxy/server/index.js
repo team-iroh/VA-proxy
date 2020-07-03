1786c6ef715c73ebc885f4f1e8b8f14660933ef8
Vasan Arul <vasankarul@gmail.com>
2020-07-03 00:12:27 -0700

    Committed code should reflect id route additions

--- server-proxy/server/index.js
+++ server-proxy/server/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = 4001;
 
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(bodyParser.json());
+
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public', 'index.html'));
+// });
 
 app.listen(port, () => console.log(`Proxy server is listening at http://localhost:${port}`));
 
